22c912ddad683f92fd8a1ca16380501a8f2b19cd
Sabir <sabirthapa19@gmail.com>
2022-09-06 18:22:03 +0545

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Sabir's MacBook on 9/6/22.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
